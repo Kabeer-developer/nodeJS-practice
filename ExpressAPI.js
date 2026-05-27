b0376b696864946ff25119663bfd4c2cc8b77a6c
Kabeer-developer <kabeeratwork7@gmail.com>
2026-05-27 19:58:14 +0530

    Express Server

--- ExpressAPI.js
+++ ExpressAPI.js
@@ -2,17 +2,12 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config();
 const app = express();
-
 app.use(express.json());
 
 app.get("/",(req,res)=> {
-    res.send("<h1>API is Running</h1>");
+    res.send("<h1>Helloo Kabeer...</h1>")
 });
 
-app.get("/users",(req,res)=> {
-    const users=["a","b","c"];
-    res.json(users);
+app.listen(process.env.PORT,()=> {
+    console.log("Server is Running");
 })
-
-
-app.listen(process.env.PORT,()=> console.log(`server is running on http://localhost:${process.env.PORT}`))
